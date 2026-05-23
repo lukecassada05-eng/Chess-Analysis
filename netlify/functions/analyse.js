ed6730408071e55f5e22089f60897ddffd9f92a0
lukecassada05-eng <lukecassada05@gmail.com>
2026-05-23 01:56:09 -0400

    Add files via upload

--- netlify/functions/analyse.js
+++ netlify/functions/analyse.js
@@ -20,7 +20,7 @@ exports.handler = async (event) => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'x-api-key': process.env.ANTHROPIC_API_KEY,
+        'x-api-key': process.env.Chess_key,
         'anthropic-version': '2023-06-01',
       },
       body: JSON.stringify(body),
